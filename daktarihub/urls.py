3fd3ad5e4b613a51b3b651aaceeeed0d274e25a1
Muraya-Pat <patrickmuraya823@gmail.com>
2025-04-26 17:29:14 +0300

    Authentication updates: register authentication updates on urls.py

--- daktarihub/urls.py
+++ daktarihub/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('clients/', include('clients.urls')),
     path('programs/', include('programs.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    
 
 ]
