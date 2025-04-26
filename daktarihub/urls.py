c1e7ec6d797ae29bb1bee2444f22f778285dfd93
Muraya-Pat <patrickmuraya823@gmail.com>
2025-04-26 22:03:25 +0300

    updates: base template and urls updates

--- daktarihub/urls.py
+++ daktarihub/urls.py
@@ -18,6 +18,17 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DaktariHub API",
+        default_version='v1',
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +38,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('api/', include('api.urls')),
-    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 
 ]
